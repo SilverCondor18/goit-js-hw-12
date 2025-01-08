174d6c5840dde2b3b5c6fa9a2a030b4a71feff00
Maksym Dzyubenko <ckraer@gmail.com>
2025-01-08 22:17:25 +0200

    Markup and style timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from "flatpickr";
+
+const picker = document.querySelector("#datetime-picker");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(picker, options);
